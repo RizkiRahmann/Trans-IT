fdeefb0333a4ff41100cb1e48dacf00aed462010
DeMan <devin.manuel58@gmail.com>
2024-02-28 15:50:13 +0700

    userService interface yang terbaru

--- src/main/java/com/pioneers/transit/service/UserService.java
+++ src/main/java/com/pioneers/transit/service/UserService.java
@@ -3,7 +3,15 @@ package com.pioneers.transit.service;
 import com.pioneers.transit.dto.request.UserRequest;
 import com.pioneers.transit.dto.response.UserResponse;
 import com.pioneers.transit.entity.User;
+import com.pioneers.transit.entity.UserCredential;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-public interface UserService {
+public interface UserService  {
+    UserCredential loadByUserId(String userId);
+
     UserResponse create(UserRequest request);
+
+    UserDetails loadUserByUsername(String email) throws UsernameNotFoundException;
 }

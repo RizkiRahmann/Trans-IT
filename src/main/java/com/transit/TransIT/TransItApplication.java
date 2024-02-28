24ebf236712e65908ba2af55102a9190201a9848
RizkiRahmann <riskirahman1116@gmail.com>
2024-02-28 11:17:46 +0700

    [FEATURE] new apps

---
+++ src/main/java/com/transit/TransIT/TransItApplication.java
@@ -0,0 +1,13 @@
+package com.transit.TransIT;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransItApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TransItApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transit/TransIT/TransItApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transit.TransIT;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransItApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

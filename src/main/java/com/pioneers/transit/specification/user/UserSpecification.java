bb15cf83aa1731a1fe5513a15336b03759078be6
niswa_ajilla <najilla19@gmail.com>
2024-03-01 10:53:50 +0700

    [FEATURE] add user specification

---
+++ src/main/java/com/pioneers/transit/specification/user/UserSpecification.java
@@ -0,0 +1,30 @@
+package com.pioneers.transit.specification.user;
+
+import com.pioneers.transit.entity.User;
+import jakarta.persistence.criteria.Predicate;
+import org.springframework.data.jpa.domain.Specification;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserSpecification {
+    public static Specification<User> getSpecification(UserSearchDTO userSearchDTO){
+        return ((root, query, criteriaBuilder) -> {
+            List<Predicate> predicates = new ArrayList<>();
+            if (userSearchDTO.getUserUsername()!=null){
+                predicates.add(criteriaBuilder.equal(root.get("username"),userSearchDTO.getUserUsername()));
+            }
+            if (userSearchDTO.getUserName()!=null){
+                predicates.add(criteriaBuilder.equal(root.get("name"),userSearchDTO.getUserName()));
+            }
+            if (userSearchDTO.getUserAddress()!=null){
+                predicates.add(criteriaBuilder.equal(root.get("address"),userSearchDTO.getUserAddress()));
+            }
+            if (userSearchDTO.getUserPhoneNumber()!=null){
+                predicates.add(criteriaBuilder.equal(root.get("phoneNumber"),userSearchDTO.getUserPhoneNumber()));
+            }
+            Predicate[] predicates1 = predicates.toArray(new Predicate[predicates.size()]);
+            return criteriaBuilder.and(predicates1);
+        });
+    }
+}

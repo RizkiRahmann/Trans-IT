f4a16f78c8c8126c0e97f501983cfa4f0622ab26
DeMan <devin.manuel58@gmail.com>
2024-03-01 16:05:28 +0700

    [FEATURE] DESTINATION SPECIFICATION

--- src/main/java/com/pioneers/transit/repository/DestinationRepository.java
+++ src/main/java/com/pioneers/transit/repository/DestinationRepository.java
@@ -2,7 +2,8 @@ package com.pioneers.transit.repository;
 
 import com.pioneers.transit.entity.Destination;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface DestinationRepository extends JpaRepository<Destination, String> {
+public interface DestinationRepository extends JpaRepository<Destination, String>, JpaSpecificationExecutor<Destination> {
 
 }

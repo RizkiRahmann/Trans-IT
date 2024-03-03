59abb801627889df81b9ae923111e82f0996492d
RizkiRahmann <riskirahman1116@gmail.com>
2024-03-03 12:12:47 +0700

    [IMPROVED] merge new coverage

--- src/main/java/com/pioneers/transit/service/impl/HotelServiceClientImpl.java
+++ src/main/java/com/pioneers/transit/service/impl/HotelServiceClientImpl.java
@@ -1,6 +1,5 @@
 package com.pioneers.transit.service.impl;
 
-import com.pioneers.transit.dto.request.HotelRequestClient;
 import com.pioneers.transit.dto.response.HotelResponseClient;
 import com.pioneers.transit.service.HotelServiceClient;
 import lombok.RequiredArgsConstructor;
@@ -9,7 +8,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.sql.Date;
 import java.util.UUID;
 
 @Service
@@ -25,11 +23,5 @@ public class HotelServiceClientImpl implements HotelServiceClient {
         );
         response.getBody().setId(UUID.randomUUID().toString());
         return response.getBody();
-    }
-
-    private static HotelResponseClient toHotelClientResponse(HotelRequestClient requestClient){
-        return HotelResponseClient.builder()
-                .result(requestClient.getResult())
-                .build();
     }
 }

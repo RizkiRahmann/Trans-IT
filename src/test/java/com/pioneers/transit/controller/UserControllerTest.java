86f208bb5c83f59a1eeff19e16bc6da362099578
DeMan <devin.manuel58@gmail.com>
2024-03-03 18:56:23 +0700

    mau narik branch upload_download

--- src/test/java/com/pioneers/transit/controller/UserControllerTest.java
+++ src/test/java/com/pioneers/transit/controller/UserControllerTest.java
@@ -11,7 +11,6 @@ import com.pioneers.transit.repository.PurchaseRepository;
 import com.pioneers.transit.repository.UserCredentialRepository;
 import com.pioneers.transit.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.aot.hint.TypeReference;
@@ -20,10 +19,8 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import java.sql.Date;
-import java.util.StringTokenizer;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,7 +32,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Slf4j
 class UserControllerTest {
     @Autowired
     private MockMvc mockMvc;
@@ -59,37 +55,18 @@ class UserControllerTest {
 
     @Test
     void createUserSuccess() throws Exception {
-        String email = "superadmin@gmail.com";
-        String password = "12345";
-
-        String body = "{\"email\":\"" + email + "\", \"password\":\"" + password + "\"}";
-
-        MvcResult result = mockMvc.perform(
-                post("/auth/login")
-                        .accept(MediaType.APPLICATION_JSON)
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(body)
-                ).andExpect(status().isOk()).andReturn();
-
-
-        String response = result.getResponse().getContentAsString();
-        response = response.replace("{\"data\": \"", "");
-        String token = response.replace("\"}", "");
-        log.info("testtt " + token);
-
         UserCredential userCredential = userCredentialRepository.findById("fe6baee9-3c3a-4867-858f-d70c46df8d1d")
-                .git orElseThrow(null);
+                .orElseThrow(null);
         UserRequest request = new UserRequest();
         request.setUsername("chiteki");
         request.setName("Rizki");
         request.setBirthDate(Date.valueOf("2222-11-1"));
         request.setAddress("Jalan");
-        request.setPhoneNumber("808010102020");
+        request.setPhoneNumber("80801010");
         request.setUserCredential(userCredential);
 
         mockMvc.perform(
                 post("/user")
-                        .header("Authorization",  token)
                         .accept(MediaType.APPLICATION_JSON)
                         .contentType(MediaType.APPLICATION_JSON)
                         .content(objectMapper.writeValueAsString(request))
